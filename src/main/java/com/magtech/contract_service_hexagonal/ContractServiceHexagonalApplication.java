1fcbfa64f21e8f664291b941ba4be9a3696a59a8
magtechnologies <celo_mag@hotmail.com>
2025-07-10 14:03:26 -0300

    Meu primeiro commit.

---
+++ src/main/java/com/magtech/contract_service_hexagonal/ContractServiceHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.magtech.contract_service_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractServiceHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractServiceHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magtech/contract_service_hexagonal/ContractServiceHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magtech.contract_service_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractServiceHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
